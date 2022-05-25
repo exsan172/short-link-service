624a1b94b53059653de3addb70d625620ab29084
Exsan Renaldhi <exsan172@gmail.com>
2022-05-25 12:01:56 +0700

    deploy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require("cors")
 const config = require("./config")
 
 const serviceRouter = require('./routes/service');
@@ -11,6 +12,10 @@ const redirectRouter = require('./routes/redirect');
 const app = express();
 
 config.dbConnection()
+const corsOptions = {
+	origin: '*',
+}
+app.use(cors(corsOptions))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
